f9b6dcacb9e572fef9b73b0d9bea7d2251d363c4
amahdevar <110831292+amahdevar@users.noreply.github.com>
2024-11-19 15:19:28 +0330

    Update AdminCalendar.jsx

--- components/Calendar/AdminCalendar.jsx
+++ components/Calendar/AdminCalendar.jsx
@@ -1,1 +1,26 @@
+import React from "react";
+import { Calendar } from "react-modern-calendar-datepicker";
+import { isPersianDate } from "../../utils/calendarUtils";
 
+const AdminCalendar = ({ options, setOptions }) => {
+  const handleDateChange = (date) => {
+    if (!isPersianDate(date)) return;
+    const key = `${date.year}-${date.month}-${date.day}`;
+    if (!options[key]) options[key] = ["", ""];
+    setOptions({ ...options });
+  };
+
+  return (
+    <div className="calendar-container">
+      <Calendar
+        value={null}
+        onChange={handleDateChange}
+        shouldHighlightWeekends
+        locale="fa" // Persian calendar
+      />
+      {/* Render editable options here */}
+    </div>
+  );
+};
+
+export default AdminCalendar;
