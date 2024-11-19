96d84b49eaa767ec2f96580b8bdbceebfc4654ff
amahdevar <110831292+amahdevar@users.noreply.github.com>
2024-11-19 15:19:38 +0330

    Update UserCalendar.jsx

--- components/Calendar/UserCalendar.jsx
+++ components/Calendar/UserCalendar.jsx
@@ -1,1 +1,27 @@
+import React from "react";
+import { Calendar } from "react-modern-calendar-datepicker";
+import { isPersianDate } from "../../utils/calendarUtils";
 
+const UserCalendar = ({ options, preferences, setPreferences }) => {
+  const handleSelect = (date) => {
+    if (!isPersianDate(date)) return;
+    const key = `${date.year}-${date.month}-${date.day}`;
+    const currentOption = preferences[key] || "";
+    const newOption = currentOption === options[key][0] ? options[key][1] : options[key][0];
+    setPreferences({ ...preferences, [key]: newOption });
+  };
+
+  return (
+    <div className="calendar-container">
+      <Calendar
+        value={null}
+        onChange={handleSelect}
+        shouldHighlightWeekends
+        locale="fa" // Persian calendar
+      />
+      {/* Render selected preferences below */}
+    </div>
+  );
+};
+
+export default UserCalendar;
