b1206a541f90e39cf77f4e3b75da303826d7a966
amahdevar <110831292+amahdevar@users.noreply.github.com>
2024-11-19 15:15:54 +0330

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,1 +1,10 @@
+import axios from "axios";
 
+const api = axios.create({
+  baseURL: "https://your-railway-backend-url.com/api",
+});
+
+export const fetchOptions = () => api.get("/options");
+export const savePreferences = (data) => api.post("/preferences", data);
+export const manageUsers = (action, userData) =>
+  api.post(`/users/${action}`, userData);
